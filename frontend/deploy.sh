9f4ee02f255866a4321f21a7294a26c88e47d861
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2023-06-25 13:20:25 +0000

    updated app.js and deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,4 +1,4 @@
 # make sure you aws profile is configured
 npm run build
-aws s3 sync /build s3://schedule-my-email
-aws cloudfront create-invalidation --distribution-id E2VGL9U1MEY --paths "/*" --no-cli-pager
+aws s3 sync /build s3://schedulemyemail
+aws cloudfront create-invalidation --distribution-id E1OIVAJ8X0IFM6 --paths "/*" --no-cli-pager
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <nav className="navbar navbar-light bg-light static-top">
         <div className="container">
           <a className="navbar-brand" href="#!">Schedule My Email</a>
-          <a className="btn btn-primary" href="#signup">Sign Up</a>
+          {/* <a className="btn btn-primary" href="#signup">Sign Up</a> */}
         </div>
       </nav>
       {/* Masthead*/}
@@ -73,29 +73,29 @@ function App() {
             <div className="col-lg-4">
               <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                 <div className="features-icons-icon d-flex"><i className="bi-window m-auto text-primary" /></div>
-                <h3>Fully Responsive</h3>
-                <p className="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                <h3>Fully Automated</h3>
+                <p className="lead mb-0">This tool is automated to send you your personalized emails at just the right time.</p>
               </div>
             </div>
             <div className="col-lg-4">
               <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                 <div className="features-icons-icon d-flex"><i className="bi-layers m-auto text-primary" /></div>
-                <h3>Bootstrap 5 Ready</h3>
-                <p className="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
+                <h3>Highly Performant</h3>
+                <p className="lead mb-0">You can send as many emails ad you like and receive them on time.</p>
               </div>
             </div>
             <div className="col-lg-4">
               <div className="features-icons-item mx-auto mb-0 mb-lg-3">
                 <div className="features-icons-icon d-flex"><i className="bi-terminal m-auto text-primary" /></div>
                 <h3>Easy to Use</h3>
-                <p className="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                <p className="lead mb-0">Simply add your details and send the emails. Easy peasy!</p>
               </div>
             </div>
           </div>
         </div>
       </section>
       {/* Image Showcases*/}
-      <section className="showcase">
+      {/* <section className="showcase">
         <div className="container-fluid p-0">
           <div className="row g-0">
             <div className="col-lg-6 order-lg-2 text-white showcase-img" style={{ backgroundImage: 'url("assets/img/bg-showcase-1.jpg")' }} />
@@ -119,9 +119,9 @@ function App() {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
       {/* Testimonials*/}
-      <section className="testimonials text-center bg-light">
+      <section className="testimonials text-center text-white">
         <div className="container">
           <h2 className="mb-5">What people are saying...</h2>
           <div className="row">
@@ -145,51 +145,6 @@ function App() {
                 <h5>Sarah W.</h5>
                 <p className="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Call to Action*/}
-      <section className="call-to-action text-white text-center" id="signup">
-        <div className="container position-relative">
-          <div className="row justify-content-center">
-            <div className="col-xl-6">
-              <h2 className="mb-4">Ready to get started? Sign up now!</h2>
-              {/* Signup form*/}
-              {/* * * * * * * * * * * * * * * **/}
-              {/* * * SB Forms Contact Form * **/}
-              {/* * * * * * * * * * * * * * * **/}
-              {/* This form is pre-integrated with SB Forms.*/}
-              {/* To make this form functional, sign up at*/}
-              {/* https://startbootstrap.com/solution/contact-forms*/}
-              {/* to get an API token!*/}
-              <form className="form-subscribe" id="contactFormFooter" data-sb-form-api-token="API_TOKEN">
-                {/* Email address input*/}
-                <div className="row">
-                  <div className="col">
-                    <input className="form-control form-control-lg" id="emailAddressBelow" type="email" placeholder="Email Address" data-sb-validations="required,email" />
-                    <div className="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:required">Email Address is required.</div>
-                    <div className="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:email">Email Address Email is not valid.</div>
-                  </div>
-                  <div className="col-auto"><button className="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
-                </div>
-                {/* Submit success message*/}
-                {/**/}
-                {/* This is what your users will see when the form*/}
-                {/* has successfully submitted*/}
-                <div className="d-none" id="submitSuccessMessage">
-                  <div className="text-center mb-3">
-                    <div className="fw-bolder">Form submission successful!</div>
-                    <p>To activate this form, sign up at</p>
-                    <a className="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                  </div>
-                </div>
-                {/* Submit error message*/}
-                {/**/}
-                {/* This is what your users will see when there is*/}
-                {/* an error submitting the form*/}
-                <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
-              </form>
             </div>
           </div>
         </div>
