899245dab641d636ac214f45e108f433be5178a5
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2023-07-01 09:56:55 +0000

    updated deploy.sh to pick the build folder

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,4 +1,4 @@
 # make sure you aws profile is configured
 npm run build
-aws s3 sync /build s3://schedulemyemail
+aws s3 sync build/ s3://schedulemyemail
 aws cloudfront create-invalidation --distribution-id E1OIVAJ8X0IFM6 --paths "/*" --no-cli-pager
